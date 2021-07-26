b2e18bbe7a28084ca5b0b36adae63406deaeee54
moong <moonki0623@naver.com>
2021-07-27 03:54:53 +0900

    [feat]: mainpage_01 search option function

---
+++ src/components/SearchOption/SearchOption.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+// import { data } from '../../pages/DummyData';
+import SubTitle from '../SubTitle/SubTitle';
+import { OptionBar } from './SearchOption.element';
+import OptionList from '../OptionList/OptionList';
+const SearchOption = ({ number, subtitle, submessage }) => {
+  return (
+    <>
+      <SubTitle number={number} subtitle={subtitle} submessage={submessage} />
+      <OptionBar>
+        <OptionList></OptionList>
+      </OptionBar>
+    </>
+  );
+};
+
+export default SearchOption;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { HomeContainer } from '../styles/HomeContainer';
 import Title from '../components/Title/Title';
+import SearchOption from '../components/SearchOption/SearchOption';
 import SelectSubject from '../components/SelectSubject/SelectSubject';
 
 const Home = () => {
   return (
     <HomeContainer>
       <Title title="개설교과목 검색"></Title>
-      {/* <SearchOption number="01" subtitle="검색옵션"></SearchOption>
-      <DetailOption number="02" subtitle="세부옵션"></DetailOption> */}
+      <SearchOption
+        number="01"
+        subtitle="검색옵션"
+        submessage="*복수선택 가능"
+      ></SearchOption>
+      {/* <DetailOption number="02" subtitle="세부옵션"></DetailOption> */}
       <SelectSubject number="03" subtitle="과목선택"></SelectSubject>
     </HomeContainer>
   );
