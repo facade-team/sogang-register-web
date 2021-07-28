79fb3a45b9b7e00b5166669f48bb38ccd881dbf2
Keep-Going-HyeonSoo <chs98105@gmail.com>
2021-07-28 18:18:59 +0900

    [feat]: 로그인/회원가입 모달 디자인변경

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,71 +1,20 @@
 import React, { useRef, useEffect, useCallback } from 'react';
-import styled from 'styled-components';
-import { MdClose } from 'react-icons/md';
-import { motion, AnimatePresence } from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
 import ModalForm from './ModalForm';
 
-const Background = styled(motion.div)`
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.6);
-  position: fixed;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 999;
-`;
+import logo from '../../assets/img/logo2.png';
 
-const ModalContainer = styled(motion.div)`
-  min-width: 400px;
-  height: fit-content;
-  padding-bottom: 50px;
-  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
-  background: #fff;
-  display: grid;
-  grid-template-rows: 1fr 4fr;
-  position: relative;
-  z-index: 10;
-  border-radius: 10px;
-`;
-
-const ModalLogo = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  background-color: #b60004;
-  color: white;
-  font-size: 30px;
-  border-radius: 10px 10px 0 0;
-`;
-
-const ModalContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  //justify-content: center;
-  margin-top: 25px;
-  align-items: center;
-  line-height: 1.8;
-`;
-
-const ModalCloseBtn = styled(MdClose)`
-  cursor: pointer;
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  width: 32px;
-  height: 32px;
-  padding: 0;
-  color: white;
-`;
-
-const modalVariant = {
-  initial: { opacity: 0 },
-  isOpen: { opacity: 1 },
-  exit: { opacity: 0 },
-};
+import {
+  Background,
+  modalVariant,
+  ModalContainer,
+  ModalLogo,
+  LogoImg,
+  LogoText1,
+  LogoText2,
+  ModalContent,
+  ModalCloseBtn,
+} from './Modal.element';
 
 const Modal = ({ showModal, setShowModal, modalType, setModalType }) => {
   const BgRef = useRef();
@@ -106,7 +55,11 @@ const Modal = ({ showModal, setShowModal, modalType, setModalType }) => {
           variants={modalVariant}
         >
           <ModalContainer>
-            <ModalLogo>서강신청 로고</ModalLogo>
+            <ModalLogo>
+              <LogoImg src={logo}></LogoImg>
+              <LogoText1>서강</LogoText1>
+              <LogoText2>신청</LogoText2>
+            </ModalLogo>
             <ModalContent>
               <ModalForm
                 handleLogin={handleLogin}
---
+++ src/components/Modal/ModalForm.element.js
@@ -0,0 +1,100 @@
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import { GradationBtnComp } from '../GradationBtn/GradationBtn.element';
+
+export const FormContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+  align-items: center;
+`;
+
+export const Title = styled.p`
+  text-align: center;
+  font-size: 24px;
+  margin-bottom: 10px;
+  color: #666;
+`;
+
+export const FormGroup = styled.div`
+  display: block;
+  width: 100%;
+  margin-bottom: 15px;
+
+  label {
+    display: block;
+    color: #666;
+    font-size: 16px;
+    margin-bottom: 5px;
+  }
+  &:focus-within label {
+    color: #7248db;
+    font-weight: 800;
+    transition: all 0.4s ease;
+  }
+
+  input {
+    padding: 12px 12px;
+    width: 100%;
+    font-size: 16px;
+    border-radius: 4px;
+    border: 1px solid #c7c7c7;
+    outline: none;
+    transition: all 0.4s ease;
+  }
+
+  &:focus-within input {
+    border-color: #7248db;
+  }
+`;
+
+export const CustomGradationBtnComp = styled(GradationBtnComp)`
+  position: inherit;
+  font-weight: 800;
+  font-size: 16px;
+  width: 80%;
+  margin: 0;
+  border-radius: 12px;
+  border: none;
+  margin-top: 15px;
+
+  &:hover {
+    cursor: pointer;
+    transform: scale(1.02) translateZ(0px);
+    transition: all 0.1s ease-in;
+  }
+
+  & + div {
+    text-align: center;
+    margin-top: 24px;
+    text-decoration: underline;
+    font-size: 14px;
+    span {
+      cursor: pointer;
+    }
+  }
+`;
+
+export const Seperator = styled.div`
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+
+  p {
+    padding: 0 15px;
+    color: #c7c7c7;
+  }
+
+  div {
+    flex: 1;
+    height: 1px;
+    background-color: #c7c7c7;
+  }
+`;
+
+export const MailAllow = styled.p`
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+  justify-content: center;
+`;
