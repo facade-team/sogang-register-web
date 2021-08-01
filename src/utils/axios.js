9515b917882afdc0ed102362a4afd55d6df67112
Keep-Going-HyeonSoo <chs98105@gmail.com>
2021-08-01 21:45:36 +0900

    [feat]: axios 설정

---
+++ src/utils/axios.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://18.119.13.125:5000';
+
+export default axios;
--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,5 +1,6 @@
 export function isEmail(asValue) {
   var regExp =
+    // eslint-disable-next-line
     /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
   return regExp.test(asValue); // 형식에 맞는 경우 true 리턴
 }
