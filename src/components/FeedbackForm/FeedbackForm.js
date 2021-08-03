07bf9f01a21672fe54699dd442efe8773d1275bc
Keep-Going-HyeonSoo <chs98105@gmail.com>
2021-08-03 23:14:48 +0900

    [feat]: 피드백/문의 페이지 리렌더링 버그 수정

--- src/components/FeedbackForm/FeedbackForm.js
+++ src/components/FeedbackForm/FeedbackForm.js
@@ -21,7 +21,7 @@ const FeedbackForm = () => {
 
   useEffect(() => {
     setForm({ ...form, email: userData.email });
-  }, [userData, form, setForm]);
+  }, [userData]);
 
   return (
     <FormContainer>
