620e444b9762667af04ee128b4042a9039b13d5b
Keep-Going-HyeonSoo <chs98105@gmail.com>
2021-08-09 19:20:15 +0900

    [fix]: 피드백 폼 텍스트 수정

--- src/components/OptionModal/OptionModal.js
+++ src/components/OptionModal/OptionModal.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Dialog from '@material-ui/core/Dialog';
+import { Button } from '@material-ui/core';
 import MuiDialogTitle from '@material-ui/core/DialogTitle';
 import MuiDialogContent from '@material-ui/core/DialogContent';
 import MuiDialogActions from '@material-ui/core/DialogActions';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import Typography from '@material-ui/core/Typography';
+
+// 라디오 메뉴
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 
 import { OptionList } from './OptionModal.element';
 
@@ -72,6 +80,12 @@ export default function OptionModal({ open, setOpen, option }) {
     setOpen(false);
   };
 
+  const [value, setValue] = React.useState('과목명');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
   return (
     <>
       {option[0] ? (
@@ -84,34 +98,59 @@ export default function OptionModal({ open, setOpen, option }) {
             {option[0].type}
           </DialogTitle>
           <DialogContent
-            style={{ width: '300px', maxHeight: '500px' }}
+            style={{ width: '400px', maxHeight: '500px' }}
             dividers
           >
-            <ul>
-              {option[0].type !== '전공/영역' && option[0].type !== '검색어'
-                ? option[0] &&
-                  option[0].data.map((item, index) => (
-                    <Typography gutterBottom key={item}>
-                      <OptionList onClick={() => handleListClick(item)}>
-                        - {item}
-                      </OptionList>
-                    </Typography>
-                  ))
-                : option[0] &&
-                  option[0].data.map((item, index) => (
-                    <Typography gutterBottom key={item.id}>
-                      <OptionList onClick={() => handleMajorListClick(item)}>
-                        - {item.text}
-                      </OptionList>
-                    </Typography>
-                  ))}
-            </ul>
+            {option[0].type !== '검색어' ? (
+              <ul>
+                {option[0].type !== '전공/영역'
+                  ? option[0] &&
+                    option[0].data.map((item, index) => (
+                      <Typography gutterBottom key={item}>
+                        <OptionList onClick={() => handleListClick(item)}>
+                          - {item}
+                        </OptionList>
+                      </Typography>
+                    ))
+                  : option[0] &&
+                    option[0].data.map((item, index) => (
+                      <Typography gutterBottom key={item.id}>
+                        <OptionList onClick={() => handleMajorListClick(item)}>
+                          - {item.text}
+                        </OptionList>
+                      </Typography>
+                    ))}
+              </ul>
+            ) : (
+              <>
+                <FormControl component="fieldset">
+                  <RadioGroup value={value} onChange={handleChange} row>
+                    <FormControlLabel
+                      value="과목명"
+                      control={<Radio />}
+                      label="과목명"
+                    />
+                    <FormControlLabel
+                      value="과목번호"
+                      control={<Radio />}
+                      label="과목코드"
+                    />
+                    <FormControlLabel
+                      value="교수명"
+                      control={<Radio />}
+                      label="교수명"
+                    />
+                  </RadioGroup>
+                </FormControl>
+
+                <DialogActions>
+                  <Button autoFocus onClick={handleClose} color="primary">
+                    검색
+                  </Button>
+                </DialogActions>
+              </>
+            )}
           </DialogContent>
-          {/* <DialogActions>
-          <Button autoFocus onClick={handleClose} color="primary">
-            Save changes
-          </Button>
-        </DialogActions> */}
         </Dialog>
       ) : null}
     </>
