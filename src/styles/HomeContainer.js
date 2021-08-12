70a2c9ef9d7c82ea1f48aea30b2864579e8c8a6c
poki <ohsang1219@naver.com>
2021-08-12 23:00:46 +0900

    [fix]: mypage height 수정

--- src/styles/HomeContainer.js
+++ src/styles/HomeContainer.js
@@ -3,12 +3,13 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   width: 100%;
-  height: auto;
+  height: 100%;
   @media screen and (max-width: 900px) {
     flex-direction: column;
     justify-content: center;
     align-items: center;
     width: 100%;
+    height: auto;
   }
 `;
 
