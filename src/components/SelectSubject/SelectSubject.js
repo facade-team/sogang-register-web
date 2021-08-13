f525da2de5606bb84a2f40bc02237369c48c52c8
Keep-Going-HyeonSoo <chs98105@gmail.com>
2021-08-13 22:58:52 +0900

    [feat]: 강의언어,대면여부 필터링기능 (아직버그있음)

--- src/components/SelectSubject/SelectSubject.js
+++ src/components/SelectSubject/SelectSubject.js
@@ -38,17 +38,90 @@ const SelectSubject = ({ number, subtitle, data, onClickCard }) => {
     setChina,
   } = useSubjectContext();
 
+  const [excludeFilter, setExcludeFilter] = useState({
+    prof: [],
+    lang: [],
+    contact: [],
+  });
+
   const handleFilterClick = (e) => {
-    console.log(e.target.id);
-    if (e.target.id === '대면') {
-      setOntact(!ontact);
-    } else if (e.target.id === '비대면') {
-      setUntact(!untact);
+    const clickedFilter = e.target.id;
+    console.log('e.target', e.target.innerText);
+
+    // 클릭한 필터가 제외필터에 있는 상태였으면, 제외필터에서 없애줌
+    // 없으면, 제외필터에 추가
+
+    // 강의언어
+    if (excludeFilter.lang.includes(clickedFilter)) {
+      const temp = excludeFilter.lang;
+      for (let i = 0; i < temp.length; i++) {
+        if (temp[i] === clickedFilter) {
+          temp.splice(i, 1);
+          i--;
+        }
+      }
+      setExcludeFilter({ ...excludeFilter, lang: temp });
+    }
+    // 추가
+    else {
+      const temp = excludeFilter.lang;
+      temp.push(clickedFilter);
+      setExcludeFilter({
+        ...excludeFilter,
+        lang: temp,
+      });
+    }
+
+    // 대면여부
+    // 제거
+    if (excludeFilter.contact.includes(clickedFilter)) {
+      const temp = excludeFilter.contact;
+      for (let i = 0; i < temp.length; i++) {
+        if (temp[i] === clickedFilter) {
+          temp.splice(i, 1);
+          i--;
+        }
+      }
+      setExcludeFilter({ ...excludeFilter, contact: temp });
+    }
+    // 추가
+    else {
+      const temp = excludeFilter.contact;
+      temp.push(clickedFilter);
+      setExcludeFilter({
+        ...excludeFilter,
+        contact: temp,
+      });
     }
   };
 
+  const filtering = (subject) => {
+    // 제외필터에 해당하면 false 로 필터링
+    if (excludeFilter.contact.includes(subject.강의언어)) {
+      return false;
+    }
+    if (excludeFilter.lang.includes(subject.대면여부)) {
+      return false;
+    }
+
+    return true;
+  };
+
+  useEffect(() => {
+    if (!profOption) {
+      setExcludeFilter({ ...excludeFilter, prof: [] });
+    }
+    if (!langOption) {
+      setExcludeFilter({ ...excludeFilter, lang: [] });
+    }
+    if (!contactOption) {
+      setExcludeFilter({ ...excludeFilter, contact: [] });
+    }
+  }, [profOption, langOption, contactOption]);
+
   return (
     <>
+      {console.log('23', excludeFilter)}
       <SubTitle number={number} subtitle={subtitle}></SubTitle>
       {data && data.length !== 0 ? (
         <Container>
@@ -91,16 +164,46 @@ const SelectSubject = ({ number, subtitle, data, onClickCard }) => {
           )}
 
           {/*강의언어 정렬옵션 렌더링*/}
-          {isSearchOption && langOption}
+          {langOption && (
+            <TagContainer2>
+              <Tag2
+                id="한국어"
+                fontSize="13"
+                bgColor="#01a3a4"
+                onClick={handleFilterClick}
+                deactive={excludeFilter.lang.includes('한국어')}
+              >
+                한국어
+              </Tag2>
+              <Tag2
+                id="영어"
+                fontSize="13"
+                bgColor="#01a3a4"
+                onClick={handleFilterClick}
+                deactive={excludeFilter.lang.includes('영어')}
+              >
+                영어
+              </Tag2>
+              <Tag2
+                id="중국어"
+                fontSize="13"
+                bgColor="#01a3a4"
+                onClick={handleFilterClick}
+                deactive={excludeFilter.lang.includes('중국어')}
+              >
+                중국어
+              </Tag2>
+            </TagContainer2>
+          )}
           {/*대면여부 정렬옵션 렌더링*/}
-          {isSearchOption && contactOption && (
+          {contactOption && (
             <TagContainer2>
               <Tag2
                 id="대면"
                 fontSize="13"
                 bgColor="#01a3a4"
                 onClick={handleFilterClick}
-                deactive={!ontact}
+                deactive={excludeFilter.contact.includes('대면')}
               >
                 대면
               </Tag2>
@@ -109,48 +212,22 @@ const SelectSubject = ({ number, subtitle, data, onClickCard }) => {
                 fontSize="13"
                 bgColor="#01a3a4"
                 onClick={handleFilterClick}
-                deactive={!untact}
+                deactive={excludeFilter.contact.includes('비대면')}
               >
                 비대면
               </Tag2>
             </TagContainer2>
           )}
 
-          {contactOption && ontact && untact && (
+          {(contactOption || langOption) && (
             <CardList>
-              {data.map((subject) => (
+              {data.filter(filtering).map((subject) => (
                 <Card
                   key={subject.subject_id}
                   subject={subject}
                   onClick={() => onClickCard(subject.subject_id)}
                 ></Card>
               ))}
-            </CardList>
-          )}
-          {contactOption && ontact && !untact && (
-            <CardList>
-              {data
-                .filter((subject) => subject.대면여부 === '대면')
-                .map((ontactSubject) => (
-                  <Card
-                    key={ontactSubject.subject_id}
-                    subject={ontactSubject}
-                    onClick={() => onClickCard(ontactSubject.subject_id)}
-                  ></Card>
-                ))}
-            </CardList>
-          )}
-          {contactOption && !ontact && untact && (
-            <CardList>
-              {data
-                .filter((subject) => subject.대면여부 === '비대면')
-                .map((ontactSubject) => (
-                  <Card
-                    key={ontactSubject.subject_id}
-                    subject={ontactSubject}
-                    onClick={() => onClickCard(ontactSubject.subject_id)}
-                  ></Card>
-                ))}
             </CardList>
           )}
         </Container>
